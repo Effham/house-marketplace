595dcd59c9c1e2ad4a6bb9572a1227a07228e8e3
effham.ali <athereffham@gmail.com>
2023-06-12 20:09:40 +0500

    Profile Listings Work

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { getAuth, updateProfile } from 'firebase/auth'
-import { doc, updateDoc } from 'firebase/firestore'
+import { doc, updateDoc, collection, getDocs, query, where, deleteDoc, orderBy } from 'firebase/firestore'
 import { db } from '../firebase.config'
-
 import { useNavigate, Link } from 'react-router-dom'
-
 import { toast } from 'react-toastify'
 import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
 import homeIcon from '../assets/svg/homeIcon.svg'
-
+import ListingItem from '../components/ListingItem'
 const Profile = () => {
 
   const auth = getAuth()
 
   const [changeDetails, setChangeDetails] = useState(false)
+  const [loading, setLoading] = useState(true)
+  const [listings, setListings] = useState(null)
+
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -24,6 +25,28 @@ const Profile = () => {
 
   const navigate = useNavigate()
 
+  useEffect(() => {
+    const fetchUserListings = async () => {
+      const listingsRef = collection(db,'listings')
+      const q = query(listingsRef,where('userRef','==',auth.currentUser.uid),orderBy('timestamp','desc'))
+      const querySnap = await getDocs(q)
+
+      const listings = []
+      querySnap.forEach((doc)=>{
+        return listings.push({
+          id:doc.id,
+          data:doc.data(),
+        })
+      })
+
+      setListings(listings)
+      setLoading(false)
+    }
+
+    fetchUserListings()
+
+  },[auth.currentUser.uid])
+
   const onLogout = () => {
     auth.signOut()
     navigate('/')
@@ -31,14 +54,14 @@ const Profile = () => {
 
   const onSubmit = async () => {
     try {
-      if(auth.currentUser.displayName != name) {
+      if (auth.currentUser.displayName != name) {
         // UPDATE DISPLAY NAME IN FIREBASE
-        await updateProfile(auth.currentUser,{
+        await updateProfile(auth.currentUser, {
           displayName: name,
         })
 
-        const userRef = doc(db,'users',auth.currentUser.uid)
-        await updateDoc(userRef,{
+        const userRef = doc(db, 'users', auth.currentUser.uid)
+        await updateDoc(userRef, {
           name,
         })
       }
@@ -54,6 +77,14 @@ const Profile = () => {
     }))
   }
 
+  const onDelete = async (listingId) => {
+      if(window.confirm('are you sure you want to delete ?')){
+        await deleteDoc(doc(db,'listings',listingId))
+      }
+      const updatedListings = listings.filter((listing) => listing.id !== listingId)
+      setListings(updatedListings)
+      toast.success('Successfully deleted listing')
+  }
 
   return <div className='profile'>
     <header className="profileHeader">
@@ -94,6 +125,24 @@ const Profile = () => {
         <p>Sell or rent your home</p>
         <img src={arrowRight} alt="arrow right" />
       </Link>
+
+      {!loading && listings?.length > 0 && (
+        <>
+          <p className="listingText">
+            Your Listings
+          </p>
+          <ul className="listingList">
+            {listings.map((listing)=>(
+              <ListingItem 
+              key={listing.id} 
+              listing={listing.data} 
+              id={listing.id}
+              onDelete={()=>onDelete(listing.id)}
+              /> 
+            ))}
+          </ul>
+        </>
+      )}
     </main>
   </div>
 }
